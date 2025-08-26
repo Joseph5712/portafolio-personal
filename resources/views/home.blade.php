e52da4fa811abdd25ecec44ba7e97550f6f7e70f
Joseph5712 <jmm7gameover@gmail.com>
2025-08-26 00:17:56 -0600

    'agregando-navbar'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Bienvenido a mi Portafolio</h1>
-    <p>Explora mis proyectos y habilidades.</p>
+    {{-- <h1>Bienvenido a mi Portafolio</h1>
+    <p>Explora mis proyectos y habilidades.</p> --}}
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PORTAFOLIO</title>
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Flowbite CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
 </head>
 <body>
-    @yield('content')
+    <header>
+        @include('partials.navbar')
+    </header>
+        
+    <main>
+        @yield('content')
+    </main>
+    
+    {{-- @include('partials.footer') --}}
 </body>
 </html>
